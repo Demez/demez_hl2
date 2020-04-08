6e4111724d2c8f97e2e2e895bbc3a20cd6024ddb
Demez <demizexp@gmail.com>
2020-04-08 18:16:56 -0400

    initial

---
+++ _create_demez_hl2.bat
@@ -0,0 +1,5 @@
+@echo off
+
+py quiver\src\devtools\qpc\qpc.py -b "base.qpc_base" -a game -r libraries -g visual_studio -m DEMEZ -mf demez_hl2 -p win32
+
+pause
---
+++ _create_libraries.bat
@@ -0,0 +1,5 @@
+@echo off
+
+py D:\projects\qpc\qpc.py -b "base.qpc_base" -a libraries -g visual_studio -m DEMEZ HL2MP -mf libraries -p win32
+
+pause
