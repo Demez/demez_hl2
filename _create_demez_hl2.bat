35f69134f58f9232d732118a7536a6dc26bf776e
Demez <demizexp@gmail.com>
2020-04-08 18:58:10 -0400

    [qpc] dont build vphysics

--- _create_demez_hl2.bat
+++ _create_demez_hl2.bat
@@ -1,5 +1,5 @@
 @echo off
 
-py quiver\src\devtools\qpc\qpc.py -b "base.qpc_base" -a game -r libraries -g visual_studio -m DEMEZ -mf demez_hl2 -p win32
+py quiver\src\devtools\qpc\qpc.py -b "base.qpc_base" -a game -r libraries vphysics -g visual_studio -m DEMEZ -mf demez_hl2 -p win32
 
 pause
