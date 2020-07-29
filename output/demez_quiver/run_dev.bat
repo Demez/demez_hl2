aa41a38b846bdc611246610ce713ac4b1db0c02e
Demez <demizexp@gmail.com>
2020-07-29 12:34:41 -0400

    [quiver] fix run_dev.bat path

--- output/demez_quiver/run_dev.bat
+++ output/demez_quiver/run_dev.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set cmd_line=-console -sw -noborder -game ..\..\output\demez_quiver
+set cmd_line=-console -sw -noborder -game ..\..\..\output\demez_quiver
 
-start "" ..\..\quiver\game\bin\win32\quiver.exe %cmd_line%
-echo ..\..\quiver\game\bin\win32\quiver.exe %cmd_line%
+start "" ..\..\branches\quiver\game\bin\win32\quiver.exe %cmd_line%
+echo ..\..\branches\quiver\game\bin\win32\quiver.exe %cmd_line%
 
 timeout 5
