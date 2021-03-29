c434735da9ac38aac059dc2b344c022b4cdcfba2
Demez <demizexp@gmail.com>
2021-03-29 15:55:17 -0400

    [qpc] setup for csgo submodule

--- _create_demez_hl2_csgo.bat
+++ _create_demez_hl2_csgo.bat
@@ -2,6 +2,6 @@
 
 @REM py qpc\qpc.py -b "qpc_scripts/base.qpc_base" -a demez_game client server -g visual_studio -m DEMEZ BASE_CSGO BASE_ASW CSGO VR -mf demez_hl2_csgo -ar i386
 @REM py qpc\qpc.py -b "qpc_scripts/base.qpc_base" -a demez_gamedlls client server -g visual_studio -m DEMEZ BASE_CSGO CSGO VR -mf demez_hl2_csgo -ar i386
-py qpc\qpc.py -b "qpc_scripts/base.qpc_base" -a demez_gamedlls game -r libraries vscript -g visual_studio -m DEMEZ BASE_CSGO CSGO VR -mf demez_hl2_csgo -ar i386
+py qpc\qpc.py -b "qpc_scripts/base.qpc_base" -a demez_gamedlls game -r libs -g visual_studio -m DEMEZ BASE_CSGO CSGO VR -mf demez_hl2_csgo -ar i386
 
 pause
