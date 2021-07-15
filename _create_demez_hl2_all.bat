1ce5434aaa67cec8b61e5e2e08e04aedacba92d5
Demez <demizexp@gmail.com>
2021-07-15 15:45:00 -0400

    remove junk bat files

--- _create_demez_hl2_all.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-py qpc\qpc.py -b "qpc_scripts/base.qpc_base" -a demez_game -g visual_studio -m DEMEZ BASE_2013 BASE_QUIVER BASE_ASW -mf demez_hl2_all -ar i386
-
-pause
--- _create_demez_hl2_quiver.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-py qpc\qpc.py -b "qpc_scripts/base.qpc_base" -a game -r libraries vphysics -g visual_studio -m DEMEZ BASE_QUIVER -mf demez_hl2_quiver -ar i386
-
-pause
